34a97b02953780b6e6e021e5ace4e379cab6be22
Muhammad Iqbal <qebamagi@gmail.com>
2022-06-06 22:17:45 +0800

    add new private dir

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['Private', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'qeba@server.my',
